e4bb71f0f488d7df49433388337fe26f2f8f1eb1
Lawrencemqy <88049715+Lawrencemqy@users.noreply.github.com>
2021-07-28 14:00:44 +0800

    Add files via upload

---
+++ Tic Tac Toe/Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by 梅清阳 on 2021/7/28.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
